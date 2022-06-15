ee7c78f107a6f4c54beb9aaa38859f427a76a28c
Jacob Hill <hilljake93@gmail.com>
2022-06-15 16:53:49 -0500

    working solution

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
-console.log("HELLO PAPA PLATOON!")
-// Your function(s) should go here that will interact with the webpage or DOM
+//Create random number(1-100) and save it to the variable 'num'
+let num = Math.floor(Math.random()*100)
+
+console.log(num)
+
+//Find a way to get input saved to a variable
+
+const guessingGame = function(){
+    let guess = +document.getElementById('guess').value
+    
+    document.getElementById("instruct").innerHTML = '';
+    if(num === guess){
+        document.getElementById("feedback").innerHTML = "You got it!";
+    }
+    if(num > guess){
+        document.getElementById("feedback").innerHTML = "Too Low";
+    }
+    if(num < guess){
+        document.getElementById("feedback").innerHTML = "Too High";   
+    }
+    document.getElementById("last-num-guess").style.display = "flex"
+    document.getElementById("last-guess").innerHTML = guess;
+    
+}
